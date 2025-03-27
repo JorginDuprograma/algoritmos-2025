e08b5380395af2b42deaefd87a3d59897fe83b74
jorginDupneu <heduardoserraohs@gmail.com>
2025-03-27 12:51:56 -0300

    codigo numerico

--- testandoC/numericos.c
+++ testandoC/numericos.c
@@ -5,8 +5,9 @@ int main(int argc, char* argv[])
 int idade = 30;
 float altura = 1.72;
 double peso = 79.85;
+
 printf("digite a idade: ");
 scanf("%d",&idade);
-printf("a idade é %d\n", idade);
+printf("a idade e %d\n", idade);
 return 0;
 }
