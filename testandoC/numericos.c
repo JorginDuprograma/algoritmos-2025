6af7f3990e61d077a20a815c04271c4c819b3d52
jorginDupneu <heduardoserraohs@gmail.com>
2025-03-27 12:50:44 -0300

    os bagulho numerico

---
+++ testandoC/numericos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+int idade = 30;
+float altura = 1.72;
+double peso = 79.85;
+printf("digite a idade: ");
+scanf("%d",&idade);
+printf("a idade é %d\n", idade);
+return 0;
+}
