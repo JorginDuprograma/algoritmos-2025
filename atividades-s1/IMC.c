ef38b0dd70b03dba2f8959282f4f3fb51331674f
JorginDuprograma <heduardoserraohs@gmail.com>
2025-03-28 23:24:02 -0300

    imc certo

--- atividades-s1/IMC.c
+++ atividades-s1/IMC.c
@@ -15,10 +15,10 @@ float IMC,peso,altura;
    printf("o valor do IMC e: %.2f\n",IMC);
 
     if (IMC < 18.5) { printf("abaixo do peso normal"); }
-    else if (IMC >= 18.5 || IMC < 25) { printf("peso normal"); }
-    else if (IMC >= 25 || IMC <30) { printf("excesso de peso"); }
-    else if (IMC >= 30 || IMC <35) { printf("obesidade tipo 1"); }
-    else if (IMC >= 35 || IMC <40) { printf("obesidade tipo 2"); }
+    else if (IMC >= 18.5 && IMC < 25) { printf("peso normal"); }
+    else if (IMC >= 25 && IMC <30) { printf("excesso de peso"); }
+    else if (IMC >= 30 && IMC <35) { printf("obesidade tipo 1"); }
+    else if (IMC >= 35 && IMC <40) { printf("obesidade tipo 2"); }
     else if (IMC >= 40) { printf("obesidade tipo 3"); }
 
  return 0;  
