a7917634e70ad2f97e71159027d9eaf6ea2d42fe
JorginDuprograma <heduardoserraohs@gmail.com>
2025-04-01 11:21:47 -0300

    att formas

---
+++ atividades-s1/formas.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+ #include <math.h>
+ Pi = 3,14
+
+ int main(int argc, char* argv[])
+{
+    float area,b,B,h
+    float triangulo,retangulo,quadrado,trapezio,losango,circulo
+    int escolha
+    int 1 = 0
+
+    printf("escolha a forma geometrica que deseja calcular: ")
+    printf("1) triangulo")
+    printf("2) retangulo")
+    printf("3) quadrado")
+    printf("4) trapezio")
+    printf("5) losango")
+    printf("6) cirulo")
+    scanf("")
+ return 0;  
+}   
