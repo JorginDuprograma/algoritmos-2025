6835d9789081ff8b0279962fff694f8afa3873ff
JorginDuprograma <heduardoserraohs@gmail.com>
2025-04-04 23:59:50 -0300

    projeto pronto

--- atividades-s1/atividade4/assistente_filme.c
+++ atividades-s1/atividade4/assistente_filme.c
@@ -4,37 +4,37 @@
 {
     int escolha,leve,intenso,terror,acao;
 
-    printf("escolha o tipo de filme: leve(1) ou intenso(2)");  
+    printf("escolha o tipo de filme leve(1) ou intenso(2): ");  
     scanf("%d",&escolha);
     if (escolha <1 || escolha >2){printf("valor invalido");}
 
-    if (escolha == 1);
+    if (escolha == 1)
     {
-        printf("escolha o genero comedia(1) ou animacao(2)");
+        printf("escolha o genero comedia(1) ou animacao(2): ");
         scanf("%d",&leve);
         if (leve <1 || leve >2){printf("escolha invalida");}
 
-        if (leve == 1){printf("assista ao filme: as branquelas");}
-        else{printf("assista ao filme: toy story");}
+        if (leve == 1){printf("assista ao filme: os farofeiros");}
+        if (leve == 2){printf("assista ao filme: como treinar o seu dragao");}
     }
-    if (escolha == 2);
+    if (escolha == 2)
     {
-        printf("escolha o genero terror(1) ou acao(2)");
+        printf("escolha o genero terror(1) ou acao(2): ");
         scanf("%d",&intenso); 
         if(intenso <1 || intenso >2 ){printf("valor invalido");}
 
-        if(intenso == 1);
+        if(intenso == 1)
         {
-         printf("terror psicologico(1) ou natural(2)") scanf("%d",&terror);
-         if(terror <1 || >2) {printf("valor invalido");}
+         printf("terror psicologico(1) ou natural(2): "); scanf("%d",&terror);
+         if(terror <1 ||terror >2) {printf("valor invalido");}
          if(terror == 1){printf("assista ao filme: corra");}
          else{printf("assista ao filme: invocacao do mal");}
         }
-        if(intenso == 2);
+        if(intenso == 2)
         {
-         printf("acao do tipo sobre-natural(1) ou realista(2)")scanf("%d",&acao);
+         printf("acao do tipo super heroi(1) ou realista(2): "); scanf("%d",&acao);
          if(acao <1 || acao >2){printf("valor invalido");} 
-         if(acao == 1){printf("assista ao filme: vingadores ultimato");}
+         if(acao == 1){printf("assista ao filme: incrivel hulk");}
          else{printf("assista ao filme: mad max");}
         }    
     }
