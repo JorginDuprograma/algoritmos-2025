302a3cd0605c9b7125dc8487cc2faefe1cd5ff79
jorginDuprograma <heduardoserraohs@gmail.com>
2025-04-11 12:50:30 -0300

    att

--- atividades-s1/atividade5/notas.c
+++ atividades-s1/atividade5/notas.c
@@ -3,14 +3,19 @@
  int main(int argc, char* argv[])
 {
  int alunos;
- float notas;
 
  printf("digite o numero de alunos na turma:");
  scanf("%d",&alunos);
- if(alunos >0)do{
-  printf("digite a nota dos alunos: ");
-  scanf("%f",&notas);
- }while (notas <= alunos);
+ float notas[alunos];
+
+ if(alunos >0)
+  { 
+   do{
+   printf("digite a nota dos alunos: ");
+   scanf("%f",&notas);
+   }while (notas <= alunos);
+  }
+ else {printf("numero de alunos invalido ");}
 
  return 0;  
 }   
